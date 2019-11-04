27ace5aa10a11c8e6b480feb7159965300d0533c
Reidond <reidond@gmail.com>
2019-11-04 20:10:39 +0200

    Add padding to right container

--- src/components/right-side/RightSide.js
+++ src/components/right-side/RightSide.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Items from './Items';
+import Comments from './Comments';
 
 const RightSide = () => (
-  <div className="container-fluid">
+  <div className="container-fluid p-4">
     <div className="row">
-      <div className="col-6" />
-      <div className="col-6" />
+      <div className="col-6">
+        <Items />
+      </div>
+      <div className="col-6">
+        <Comments />
+      </div>
     </div>
   </div>
 );
