713e3c49ba84fc76552835d675448c01d2dc90fd
Reidond <reidond@gmail.com>
2019-11-04 19:24:54 +0200

    Add Left and Right side of app
    * Complete left side

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import LeftSide from './components/LeftSide';
+import RightSide from './components/RightSide';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="container-fluid h-100">
+      <div className="row h-100 fixed-top">
+        <div className="col-3 hidden-md-down left-side">
+          <LeftSide />
+        </div>
+        <div className="col-9 col-sm-10 col-md-10 col-lg-8 col-xl-8">
+          <RightSide />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/RightSide.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RightSide = () => <h1>RightSide</h1>;
+
+export default RightSide;
