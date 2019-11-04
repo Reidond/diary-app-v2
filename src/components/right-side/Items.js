17dd9c92f5caa64eab6ee03ec596a4d6f5468f68
Reidond <reidond@gmail.com>
2019-11-04 20:10:59 +0200

    Format files

---
+++ src/components/right-side/Items.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Items = () => (
+  <div className="card shadow-sm bg-white rounded">
+    <div className="card-body">
+      <h2 className="card-title right-side-title">Items</h2>
+    </div>
+  </div>
+);
+
+export default Items;
