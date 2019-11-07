23718ca1973d963ae513b79d2e7fa2625f46aad4
Reidond <reidond@gmail.com>
2019-11-07 11:14:21 +0200

    Remove comments reducer

--- src/reducers/commentsReducer.js
+++
@@ -1,8 +0,0 @@
-const handlers = {
-  DEFAULT: state => state
-};
-
-export default function commentsReducer(state = [], action) {
-  const handler = handlers[action.type] || handlers.DEFAULT;
-  return handler(state, action);
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import itemsReducer from './itemsReducer';
-import commentsReducer from './commentsReducer';
 
 export default combineReducers({
-  items: itemsReducer,
-  comments: commentsReducer
+  items: itemsReducer
 });
