06975f880852edd33c55f59af7760110ed7fd8ad
Reidond <reidond@gmail.com>
2019-11-09 21:29:13 +0200

    Updates

---
+++ precache-manifest.fb454b5387e481db6416d71781a3953b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d6913aabd0df325f488b601ed24efb",
+    "url": "/diary-app-v2/index.html"
+  },
+  {
+    "revision": "b4f91408561176cb3d72",
+    "url": "/diary-app-v2/static/css/main.a83b02e6.chunk.css"
+  },
+  {
+    "revision": "7f8e9b9d49e49789b959",
+    "url": "/diary-app-v2/static/js/2.10899d53.chunk.js"
+  },
+  {
+    "revision": "b4f91408561176cb3d72",
+    "url": "/diary-app-v2/static/js/main.9ccddab9.chunk.js"
+  },
+  {
+    "revision": "4e71d8d78ee296d1ff66",
+    "url": "/diary-app-v2/static/js/runtime-main.be0fa168.js"
+  }
+]);
