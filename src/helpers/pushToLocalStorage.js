e97f22702bb356e3616f244774eab7119688312d
Reidond <reidond@gmail.com>
2019-11-09 21:23:58 +0200

    Remove unused helpers

--- src/helpers/pushToLocalStorage.js
+++
@@ -1,6 +0,0 @@
-export function pushToLocalStorage(entry, item) {
-  const localItems = JSON.parse(localStorage.getItem(entry)) || [];
-  localItems.push(item);
-  localStorage.setItem(entry, JSON.stringify(localItems));
-  return localItems;
-}
